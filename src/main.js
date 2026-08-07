6ab4b955049e568416378074f5e0a65d07d26345
Pedro <contactilevex@gmail.com>
2026-08-07 20:58:40 -0300

    add Google Analytics tracking

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
 
 //CSS
 import './assets/reset.css'
 import './assets/main.css'
 
 const app = createApp(App)
+
+// Configuração do Google Analytics
+app.use(VueGtag, {
+  config: { id: 'G-458PLV96VT' }
+}, router)
 
 app.use(createPinia())
 app.use(router)
